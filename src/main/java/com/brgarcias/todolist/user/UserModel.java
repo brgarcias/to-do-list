feb899045e0f485d909f06e01ab121fab6b7f874
bruno.garcia <bruno.silva@trocafone.com>
2023-10-09 18:15:47 -0300

    feat: implementing user controller

---
+++ src/main/java/com/brgarcias/todolist/user/UserModel.java
@@ -0,0 +1,20 @@
+package com.brgarcias.todolist.user;
+
+public class UserModel {
+
+    private String username;
+    private String name;
+    private String password;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
